8989202e5b848fcdca903bb58ee3e556ae2bfc36
Uladzislau Zyshchuk <uladzislau.zyshchuk@instinctools.com>
2022-09-15 21:13:12 +0300

    init store and base reducer

--- src/components/CryptoTable/index.tsx
+++ src/components/CryptoTable/index.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../hooks/redux";
+import { fetchCryptoList } from "../../store/reducers/actionCreator";
 
 
 
 const CryptoTable: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const { cryptoList, isLoading, error } = useAppSelector(state => state.cryptoReducer);
+
+  useEffect(() => {
+    dispatch(fetchCryptoList())
+  }, [])
+
+  const buttonHandler = () => {
+    console.log(cryptoList);
+  }
+
   return (
     <>
+      <button onClick={buttonHandler}> Вывести в консоль список криптовалют</button>
     </>
   )
 }
