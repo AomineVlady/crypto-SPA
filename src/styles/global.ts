1bf0b2c1fa717bae3baefb9dba27fe361223bdea
Uladzislau Zyshchuk <uladzislau.zyshchuk@instinctools.com>
2022-09-17 18:10:34 +0300

    fix global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -13,11 +13,8 @@ export default createGlobalStyle`
 
   body{
     background-color: #C3F8FF;
-    position:fixed;
-    left:0;
-    right:0;
-    top:0;
-    bottom:0;
-    overflow-y:auto;
+    min-height: 100vh;
+    width: 100%;
+    overflow-y: auto;
   }
 `;
